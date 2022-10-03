903c56660004dbceb454b62c322ce3fef17b774f
Rajab <rkarume@gmail.com>
2022-10-03 05:59:19 +0300

    fetched data

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import BotCard from "./BotCard";
 
 function BotCollection() {
-  // Your code here
+  const [bots, setBots] = useState([])
+
+  useEffect(()=>{
+    fetch ("http://localhost:8002/bots")
+    .then ((responce)=>responce.json())
+    .then ((data)=>setBots(data))
+
+  },[])
+  
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
+        {bots.map((bot)=>(
+          < BotCard 
+          bot ={bot} 
+          key = {bot.id}
+          />
+        ))}
         Collection of all bots
       </div>
     </div>
